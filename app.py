9fa3db0fdd860d9711525de5f028892df162c4ca
DandaUpendarReddy <121006921+DandaUpendarReddy@users.noreply.github.com>
2023-02-25 10:27:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("HELLO EVERYONE")
+st.markdown(''' I am Danda Upendar Reddy an aspiring Data scientist.\n
+Github:["Follow me on github"](https://github.com/DandaUpendarReddy).\n
+Linkdein:["Connect with on Linkdein"](https://in.linkedin.com/in/danda-upendar-reddy)''')

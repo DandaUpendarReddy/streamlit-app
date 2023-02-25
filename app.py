076161b61cbba6cbd486b9f791c0ab876d49e951
DandaUpendarReddy <121006921+DandaUpendarReddy@users.noreply.github.com>
2023-02-25 10:33:46 +0530

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.title("HELLO EVERYONE")
-st.markdown(''' I am Danda Upendar Reddy an aspiring Data scientist.\n
-Github:["Follow me on github"](https://github.com/DandaUpendarReddy).\n
-Linkdein:["Connect with on Linkdein"](https://in.linkedin.com/in/danda-upendar-reddy)''')
